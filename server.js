b15ee699de422de11e6bc0453417713c0f44f118
amine <amine.hamdi@tech-instinct.com>
2019-04-11 18:45:37 +0100

    add url validity to the endpoint

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
-'use strict';
+"use strict";
 
-var express = require('express');
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
-
-var cors = require('cors');
-
+var express = require("express");
+var mongo = require("mongodb");
+var mongoose = require("mongoose");
+const { isUrlValid } = require("./checkUrlFormat");
+var cors = require("cors");
+var bodyParser = require("body-parser");
 var app = express();
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// Basic Configuration 
+// parse application/json
+app.use(bodyParser.json());
+// Basic Configuration
 var port = process.env.PORT || 3000;
 
-/** this project needs a db !! **/ 
+/** this project needs a db !! **/
+
 // mongoose.connect(process.env.MONGOLAB_URI);
 
 app.use(cors());
@@ -19,19 +24,24 @@ app.use(cors());
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
 
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function(req, res){
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-  
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your first API endpoint...
+app.post("/api/shorturl/new", async (req, res) => {
+  const newLongUrl = req.body.url;
+
+  let isValid = await isUrlValid(newLongUrl);
+  if (isValid) {
+    res.json({ message: "success" });
+  } else {
+    res.json({ error: "invalid URL" });
+  }
 });
 
-
-app.listen(port, function () {
-  console.log('Node.js listening ...');
+app.listen(port, function() {
+  console.log("Node.js listening in " + port);
 });
